de8d203f22710045bf10581e8cd269f7548f43da
MolchanovSergii <molchanse@gmail.com>
2023-06-11 15:07:38 +0300

    modal

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,23 +1,70 @@
+import React, { Component } from 'react';
 import {
   StyledImageGallery,
   StyledImageGalleryItem,
   StyledItemImage,
+  OverlayStyled,
+  ModalStyled,
 } from './ImageGalleryStyled';
 
-const ImageGallery = ({ images }) => {
-  return (
-    <StyledImageGallery>
-      {images.map(image => {
-        const { id, webformatURL, tags } = image;
-        // largeImageURL,
-        return (
-          <StyledImageGalleryItem key={id}>
-            <StyledItemImage src={webformatURL} alt={tags} />
-          </StyledImageGalleryItem>
-        );
-      })}
-    </StyledImageGallery>
-  );
-};
+class ImageGallery extends Component {
+  state = {
+    selectedImage: null,
+  };
+
+  images = this.props.images;
+
+  openModal = image => {
+    this.setState({
+      selectedImage: image,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      selectedImage: null,
+    });
+  };
+
+  handleKeyDown = event => {
+    if (event.keyCode === 27) {
+      this.closeModal();
+    }
+  };
+
+  render() {
+    const { selectedImage } = this.state;
+
+    return (
+      <div>
+        <StyledImageGallery>
+          {this.images.map(image => {
+            const { id, webformatURL, tags } = image;
+            return (
+              <StyledImageGalleryItem
+                key={id}
+                onClick={() => this.openModal(image)}
+              >
+                <StyledItemImage src={webformatURL} alt={tags} />
+              </StyledImageGalleryItem>
+            );
+          })}
+        </StyledImageGallery>
+
+        {selectedImage && (
+          <OverlayStyled
+            onClick={this.closeModal}
+            onKeyDown={this.handleKeyDown}
+            tabIndex={0}
+          >
+            <ModalStyled>
+              <img src={selectedImage.largeImageURL} alt={selectedImage.tags} />
+            </ModalStyled>
+          </OverlayStyled>
+        )}
+      </div>
+    );
+  }
+}
 
 export default ImageGallery;
